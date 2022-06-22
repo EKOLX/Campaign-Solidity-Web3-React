b5b1a132abdf6cbafb307219b28e0fe5d206d01f
EL Khan <elkhanmursali@gmail.com>
2022-06-22 18:41:16 +0400

    Creating Campaign page and completing it.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x9e1b8ceC0B767f4387f38f4868698245b972DD3A"
+  "0xa7259790574453a9ABDA5D66FD6933BBf59Ee15D"
 );
 
 export default instance;
